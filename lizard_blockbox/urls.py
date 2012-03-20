f15a42c4992f0ac2182c60136e25e8aad4539e4a
Roland van Laar <roland@micite.net>
2012-03-20 15:45:19 +0100

    Added a sample map via wms to work on the layout.

--- lizard_blockbox/urls.py
+++ lizard_blockbox/urls.py
@@ -1,16 +1,22 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
-from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import url
 from django.contrib import admin
 
 from lizard_ui.urls import debugmode_urlpatterns
+from lizard_maptree.views import MaptreeHomepageView
 
 admin.autodiscover()
 
+ITEM_MODELS = ['']
+
 urlpatterns = patterns(
     '',
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',
+        MaptreeHomepageView.as_view(),
+        {'item_models': ITEM_MODELS},
+        name='lizard_wms.homepage'),
+    #url(r'^admin/', include(admin.site.urls)),
     # url(r'^something/',
     #     direct.import.views.some_method,
     #     name="name_it"),
