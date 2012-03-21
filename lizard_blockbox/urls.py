e3ccb7d2abae78e34150387ebb17f8ba3eaa884e
Roland van Laar <roland@micite.net>
2012-03-21 11:04:28 +0100

    Added blockbox view, url and template.

--- lizard_blockbox/urls.py
+++ lizard_blockbox/urls.py
@@ -1,24 +1,16 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
 from django.conf.urls.defaults import patterns
 from django.conf.urls.defaults import url
-from django.contrib import admin
 
 from lizard_ui.urls import debugmode_urlpatterns
-from lizard_maptree.views import MaptreeHomepageView
+from lizard_blockbox.views import BlockboxHome
 
-admin.autodiscover()
-
-ITEM_MODELS = ['']
 
 urlpatterns = patterns(
     '',
     url(r'^$',
-        MaptreeHomepageView.as_view(),
-        {'item_models': ITEM_MODELS},
-        name='lizard_wms.homepage'),
-    #url(r'^admin/', include(admin.site.urls)),
-    # url(r'^something/',
-    #     direct.import.views.some_method,
-    #     name="name_it"),
+        BlockboxHome.as_view(),
+        {},
+        name='lizard_blockbox.homepage'),
     )
 urlpatterns += debugmode_urlpatterns()
--- lizard_blockbox/views.py
+++ lizard_blockbox/views.py
@@ -1,3 +1,11 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
 
-# Create your views here.
+from lizard_map.views import MapView
+
+
+class BlockboxHome(MapView):
+    """
+    Test homepage for blockbox.
+    """
+
+    template_name = "lizard_blockbox/homepage.html"
