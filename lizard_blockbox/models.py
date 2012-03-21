7efd4ddb93bc29875a8b386e906f5970ed6b0585
Roland van Laar <roland@micite.net>
2012-03-21 13:35:36 +0100

    BlockboxShape is now a meta class since it's probably not going to be used because there isn't a one on one correlation between data and shape files.

--- lizard_blockbox/models.py
+++ lizard_blockbox/models.py
@@ -1,4 +1,25 @@
 # (c) Nelen & Schuurmans.  GPL licensed, see LICENSE.txt.
-# from django.db import models
+from django.db import models
 
-# Create your models here.
+from lizard_shape import models as shape_models
+
+
+class BlockboxShape(shape_models.Shape):
+    """Abstract class for adding data to shapefiles.
+
+    Abstract for now
+    """
+    data_file = models.FileField(upload_to=shape_models.UPLOAD_TO,
+                                 blank=True)
+
+    def save(self, *args, **kwargs):
+        if self.data_file:
+            data_base_name = self.date_file.name.rpartition('.')
+            shp_base_name = self.shp_file.name.rpartition('.')
+            if data_base_name != shp_base_name:
+                raise shape_models.ShapeNameError(
+                    "Uploaded files do not have common filename base.")
+        return super(BlockboxShape, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
