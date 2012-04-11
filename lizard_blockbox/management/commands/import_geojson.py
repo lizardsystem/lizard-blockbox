e18b5a42a27d910ee0df501a850aa27289b4b3a3
Roland van Laar <roland@micite.net>
2012-04-11 10:25:39 +0200

    Add geojson data import for measurements.

---
+++ lizard_blockbox/management/commands/import_geojson.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand
+from django.utils import simplejson as json
+
+from lizard_blockbox import models
+
+
+class Command(BaseCommand):
+    args = '<geojson geojson ...>'
+    help = "Imports the measure geojson file"
+
+    def handle(self, *args, **options):
+        map(self.parse, args)
+
+    def parse(self, json_file):
+        data = json.load(open(json_file, 'rb'))
+        features = data['features']
+        for feature in features:
+            properties = feature['properties']
+            # Parse code key dynamically
+            # Key can be Code_IVM or Code_QS, or something else.
+            short_name_key = [i for i in properties.keys()
+                              if i.lower().startswith('code')][0]
+
+            # Parse and save the properties
+            try:
+                measure = models.Measure.objects.get(
+                    short_name=properties[short_name_key])
+            except models.Measure.DoesNotExist:
+                print properties[short_name_key]
+            else:
+                measure.name = properties['titel']
+                measure.measure_type = properties['type']
+                measure.traject = properties['traject']
+                #XXX: Be aware that the River Reach must be coupled with
+                # the measure
+                measure.km_from = int(round(properties['km_van']))
+                measure.km_to = int(round(properties['km_tot']))
+                measure.save()
