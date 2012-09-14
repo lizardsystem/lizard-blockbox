a99969280260c2f8de6e1c50f42230dd2eb4463b
Reinout van Rees <reinout@vanrees.org>
2012-09-14 10:03:49 +0000

    Added more models to the admin to help with debugging. (And yes, I found the problem this way :-) )

--- lizard_blockbox/admin.py
+++ lizard_blockbox/admin.py
@@ -4,6 +4,9 @@ from lizard_blockbox import models
 
 
 admin.site.register(models.Reach)
+admin.site.register(models.RiverSegment)
 admin.site.register(models.NamedReach)
 admin.site.register(models.Measure)
 admin.site.register(models.WaterLevelDifference)
+admin.site.register(models.SubsetReach)
+# admin.site.register(models.CityLocation)
