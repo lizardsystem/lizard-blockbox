7f4182180ac25dacdcc06729ce9fed5460ba3a6d
Roland van Laar <roland@micite.net>
2012-10-15 14:11:11 +0200

    Removed a debugging try/except

--- lizard_blockbox/views.py
+++ lizard_blockbox/views.py
@@ -248,28 +248,24 @@ class BlockboxView(MapView):
         return [field['label'] for field in measure.pretty()]
 
     def measures(self):
-        try:
-            measures_ids = namedreach2measures(_selected_river(self.request))
-            measures = models.Measure.objects.filter(short_name__in=measures_ids)
-            selected_measures = _selected_measures(self.request)
-            available_factsheets = _available_factsheets()
+        measures_ids = namedreach2measures(_selected_river(self.request))
+        measures = models.Measure.objects.filter(short_name__in=measures_ids)
+        selected_measures = _selected_measures(self.request)
+        available_factsheets = _available_factsheets()
         # selected_river = _selected_river(self.request)
-            result = []
-            for measure_obj in measures:
-                measure = {}
-                measure['fields'] = measure_obj.pretty()
-                measure['selected'] = measure_obj.short_name in selected_measures
-                measure['name'] = unicode(measure_obj)
-                measure['short_name'] = measure_obj.short_name
-                if measure_obj.short_name in available_factsheets:
-                    measure['pdf_link'] = reverse(
-                        'measure_factsheet',
-                        kwargs={'measure': measure_obj.short_name})
-                result.append(measure)
-            print("length: %s" %len(result))
-            return result
-        except Exception as E:
-            import ipdb; ipdb.set_trace()
+        result = []
+        for measure_obj in measures:
+            measure = {}
+            measure['fields'] = measure_obj.pretty()
+            measure['selected'] = measure_obj.short_name in selected_measures
+            measure['name'] = unicode(measure_obj)
+            measure['short_name'] = measure_obj.short_name
+            if measure_obj.short_name in available_factsheets:
+                measure['pdf_link'] = reverse(
+                    'measure_factsheet',
+                    kwargs={'measure': measure_obj.short_name})
+            result.append(measure)
+        return result
 
     @property
     def legends(self):
