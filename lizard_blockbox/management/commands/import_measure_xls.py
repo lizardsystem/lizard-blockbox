4c8a2750fe6ee5752be8cc2d945ecbb65a3ff6dc
Carsten Byrman <carsten.byrman@nelen-schuurmans.nl>
2014-11-17 17:34:51 +0100

    Save WIP.

--- lizard_blockbox/management/commands/import_measure_xls.py
+++ lizard_blockbox/management/commands/import_measure_xls.py
@@ -1,5 +1,7 @@
 from optparse import make_option
 import logging
+import os
+import re
 import sys
 
 from django.core.management.base import BaseCommand
@@ -10,8 +12,8 @@ logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
-    args = '<excelfile excelfile ...>'
-    help = ("Imports the measure excelfile, "
+    args = '<directory>'
+    help = ("Imports measure excelfiles, "
             "use --flush to flush the previous imports.")
 
     option_list = BaseCommand.option_list + (
@@ -28,9 +30,33 @@ class Command(BaseCommand):
 
         if len(args) == 0:
             if not flush:
-                print "Pass excel files as arguments."
+                print "Pass a directory as argument."
                 sys.exit(1)
             sys.exit(0)
 
-        for excelpath in args:
+        d = args[0]  # directory
+
+        if not os.path.isdir(d):
+            print "Pass a directory as argument."
+            sys.exit(1)
+
+        for excelpath in self.latest(d):
             import_helpers.import_measure_xls(excelpath, self.stdout)
+
+    def latest(self, d):
+        """Return a list of the latest version of each file in a directory.
+
+        First, get all files like {name}_v{number}.xls. Then, for each {name}
+        determine the latest {number}. Add the corresponding file to a list.
+
+        """
+        files = [f for f in os.listdir(d) if re.match(r'.+_v\d+\.xls', f)]
+        names = set([f.rsplit('_v', 1)[0] for f in files])
+        files = []
+        for name in names:
+            f = sorted([
+                f for f in os.listdir(d)
+                if re.match(name + r'_v\d+\.xls', f)
+            ])[-1]
+            files.append(os.path.join(d, f))
+        return files
